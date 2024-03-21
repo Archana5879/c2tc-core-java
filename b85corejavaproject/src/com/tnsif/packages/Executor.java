90673f8a1d340883a3750ab65f3030429543dea7
user <user@DESKTOP-2K7FBML>
2024-03-22 00:19:38 +0530

    First commit

--- b85corejavaproject/src/com/tnsif/packages/Executor.java
+++ b85corejavaproject/src/com/tnsif/packages/Executor.java
@@ -3,7 +3,7 @@ package com.tnsif.packages;
 public class Executor {
 
 	public static void main(String[] args) {
-		 Student s1=	new Student();
+		 Student s1=new Student();
 		 System.out.println(s1);
 
 	}
---
+++ b85corejavaproject/src/employees/Developer.java
@@ -0,0 +1,13 @@
+package employees;
+
+public class Developer extends Employee {
+	private String programmingLanguage;
+
+	public String getProgrammingLanguage() {
+		return programmingLanguage;
+	}
+
+	public void setProgrammingLanguage(String programmingLanguage) {
+		this.programmingLanguage = programmingLanguage;
+	}
+}
