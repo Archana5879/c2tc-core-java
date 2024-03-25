fe9460e792e50fd5e79d6737d72c8d47d9a7b618
user <user@DESKTOP-2K7FBML>
2024-03-25 20:23:49 +0530

    First commit

---
+++ b85corejavaproject/src/com/tnsif/demo/SLInheritance.java
@@ -0,0 +1,10 @@
+package com.tnsif.demo;
+
+public class SLInheritance {
+
+	public static void main(String[] args) {
+      Student obj=new Student("Archana","8976543213","Bangalore",123456795,101,"EWIT");
+      System.out.println(obj);
+	}
+
+}
